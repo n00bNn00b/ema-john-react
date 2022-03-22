06de16afd08c3d0f50a4d249202f0492ec1814af
n00bNn00b <reldewan9@gmail.com>
2022-03-22 19:20:23 +0600

    product align fixed with vanila css

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -7,7 +7,16 @@ const Product = (props) => {
   return (
     <div className="product">
       <img src={img} alt="" />
-      <p className="product-name">{name}</p>
+      <div className="product-info">
+        <p className="product-name">{name}</p>
+        <p>Price: {price} </p>
+        <p>
+          <small>Seller: {seller} </small>
+        </p>
+        <p>
+          <small>Ratings: {ratings} </small>
+        </p>
+      </div>
     </div>
   );
 };
